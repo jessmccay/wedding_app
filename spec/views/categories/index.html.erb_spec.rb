f8910cfc2d04a314fcf1b2747dfcef21ad8a45f6
Mrinalini Sinha <ms6471@nyu.edu>
2017-02-24 11:25:11 -0800

    added categories scaffold

---
+++ spec/views/categories/index.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "categories/index", type: :view do
+  before(:each) do
+    assign(:categories, [
+      Category.create!(
+        :name => "Name",
+        :color => "Color",
+        :descripton => "MyText",
+        :user => nil
+      ),
+      Category.create!(
+        :name => "Name",
+        :color => "Color",
+        :descripton => "MyText",
+        :user => nil
+      )
+    ])
+  end
+
+  it "renders a list of categories" do
+    render
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Color".to_s, :count => 2
+    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "tr>td", :text => nil.to_s, :count => 2
+  end
+end
