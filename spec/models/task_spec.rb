82badb2ccbbd190f4938c0cf910c31a70521f79d
cpena002 <carlos_enrique_pena@yahoo.com>
2017-04-07 13:04:12 -0700

    Unit tested task model

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can be saved to the db" do
+    user = User.new(username: "fake", email: "fake@fake.com", password: "faking", password_confirmation: "faking")
+    user.save
+    flowers = Category.new(name: "flowers", user_id: user.id)
+    flowers.save
+    task1 = Task.new(name: "call florist", category_id: flowers.id)
+    expect(task1.save).to be true
+  end
 end
