fe93cbf4cf5ad4db0bc5ec65eb19ee8acb013af1
jessmccay <jessmccay@gmail.com>
2017-05-16 22:25:43 -0700

    accordian menu for task categories, fix table column in database for categories descripton to description

--- db/schema.rb
+++ db/schema.rb
@@ -18,10 +18,10 @@ ActiveRecord::Schema.define(version: 20170407190613) do
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.string   "color"
-    t.text     "descripton"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "description"
     t.index ["user_id"], name: "index_categories_on_user_id", using: :btree
   end
 
