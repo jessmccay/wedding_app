11e89d60e7a73b4fc7f429559fac90a18d9866b8
jessmccay <jessmccay@gmail.com>
2017-10-03 00:39:25 -0700

    users cannot choose past date for wedding date

--- spec/features/landing_pages_spec.rb
+++ spec/features/landing_pages_spec.rb
@@ -6,13 +6,13 @@ RSpec.feature "LandingPages", type: :feature do
       Given "I am on the landing page" do
         visit "/"
       end
-      Then "I can see a log in form" do
+      And "I can see a log in form" do
         expect(page).to have_content "Log in"
       end
-      And "I can sign up as a new user" do
+      Then "I can sign up as a new user" do
         click_on "Sign up"
       end
-      Then "I can fill out the sign up form" do
+      Then "I can successfully fill out the sign up form" do
         fill_in "Email", with: "mpsdevs@gmail.com"
         fill_in "Username", with: "mps"
         fill_in "Password", with: "password;)"
@@ -23,6 +23,7 @@ RSpec.feature "LandingPages", type: :feature do
         expect(page).to have_content "The countdown begins!"
         click_on "Logout"
       end
+
       Given "I created an account, I can log back in" do
         visit '/'
         expect(page).to have_content("Log in")
