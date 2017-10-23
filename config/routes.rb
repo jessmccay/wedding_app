ae30ffb4d0a7ee579eb324413006563c0c610598
jessmccay <jessmccay@gmail.com>
2017-10-22 18:18:27 -0700

    implementation of google calendar API

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
 
+  get '/redirect', to: 'calendar#redirect', as: 'redirect'
+  get '/callback', to: 'calendar#callback', as: 'callback'
   resources :tasks
   get "tasks/:id/complete_task" => "tasks#complete_task"
-
+  get '/calendars', to: 'calendar#calendars', as: 'calendars'
   resources :categories
 
   devise_for :users
+
+  get '/events/:calendar_id', to: 'calendar#events', as: 'events', calendar_id: /[^\/]+/
+  get '/calendar/calendars'
 
   get 'welcome/index'
   root 'welcome#index'
